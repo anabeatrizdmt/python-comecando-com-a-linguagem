d8341c23aeb810265614ce4b98e2d3d480e246de
Ana Beatriz Trindade <108698090+anabeatrizdmt@users.noreply.github.com>
2022-09-09 17:59:14 -0300

    repositorio curso python alura

---
+++ adivinhacao.py
@@ -0,0 +1,26 @@
+print("*********************************")
+print("Bem-vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+print("Você digitou", chute_str)
+
+chute = int(chute_str)
+
+acertou = chute == numero_secreto
+chute_maior = chute > numero_secreto
+chute_menor = chute < numero_secreto
+
+if(acertou):
+    print("Você acertou!")
+elif(chute_maior):
+    print("Você errou! O seu chute foi maior do que o numero secreto.")
+elif(chute_menor):
+    print("Você errou! O seu chute foi menor do que o numero secreto.")
+else:
+    print("error")
+
+print("Fim do jogo")
