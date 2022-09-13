03470c2d34a8773611d048d00719e00e1f9345db
Ana Beatriz Trindade <108698090+anabeatrizdmt@users.noreply.github.com>
2022-09-12 22:43:07 -0300

    trocando 'while' pelo 'for'

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,39 +3,37 @@ import random
 print("*********************************")
 print("Bem-vindo ao jogo de adivinhação!")
 print("*********************************")
-print("Adivinhe o número secreto entre 1 e 10")
-print("*********************************")
+
 
 numero_secreto = random.randrange(1, 11)
 
-total_de_tentativa = 3
-rodada = 1
-chute=99999
+total_de_tentativas = 3
+chute = 99999
 
-while(rodada<=total_de_tentativa):
-    if(chute != numero_secreto):
-        print("Tentativa {} de {}".format(rodada,total_de_tentativa))
-        chute_str = input("Digite o seu número: ")
-        print("Você digitou", chute_str)
-        chute = int(chute_str)
+for rodada in range(1,total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 10: ")
+    print("Você digitou", chute_str)
+    chute = int(chute_str)
 
-        acertou = chute == numero_secreto
-        chute_maior = chute > numero_secreto
-        chute_menor = chute < numero_secreto
+    if(chute < 1 or chute > 10):
+        print("Você deve digitar um número entre 1 e 10.")
+        continue
 
-        if(acertou):
-            print("Você acertou!")
-        elif(chute_maior):
-            print("Você errou! O seu chute foi maior do que o numero secreto.")
-        elif(chute_menor):
-            print("Você errou! O seu chute foi menor do que o numero secreto.")
-        else:
-            print("error")
+    acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
-        rodada += 1
-
-    else:
+    if(acertou):
+        print("Você acertou!")
         break
+    elif(chute_maior):
+        print("Você errou! O seu chute foi maior do que o numero secreto.")
+    elif(chute_menor):
+        print("Você errou! O seu chute foi menor do que o numero secreto.")
+    else:
+        print("error")
+
 
 print("O numero secreto era", numero_secreto)
 print("Fim do jogo")
